fc8e61649b82f9290c7ce0b91c3445a454176a5a
hannapodeszwa <hannapodeszwa@gmail.com>
2021-06-21 21:37:03 +0200

    przyciskow duzo bez scrolla

--- app/src/main/java/pl/polsl/waga/AllProducts.kt
+++ app/src/main/java/pl/polsl/waga/AllProducts.kt
@@ -2,8 +2,9 @@ package pl.polsl.waga
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_all_products.*
 
 
 class AllProducts : AppCompatActivity() {
@@ -14,8 +15,38 @@ class AllProducts : AppCompatActivity() {
 
         val exitButton: Button = findViewById(R.id.exitButton)
         exitButton.setOnClickListener {
+            createLayoutDynamically(10);
             val myIntent = Intent(this, MainActivity::class.java)
             startActivity(myIntent)
+        }
+        /////////////////////////////
+        createLayoutDynamically(10);
+        ///////////////////////////
+    }
+    private fun createLayoutDynamically(n: Int) {
+       // val baseLayout = findViewById<TableLayout>(R.id.table)
+        val baseLayout = findViewById<LinearLayout>(R.id.linearLayout)
+        val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+        for (i in 0..n) {
+            var myButton:Button = Button(this);
+            myButton.setText("Button :"+i);
+            myButton.setId(i);
+           // myButton.width = 10 + i
+            //myButton.height =20 + i
+            myButton.x = 10.0f + i*25.0f
+            myButton.y = 10.0f + i*25.0f
+            var id_ = myButton.getId();
+
+            //val ll = LinearLayout(R.id.linearLayout)
+            baseLayout.addView(myButton);
+           // ll.addView(myButton, lp)
+
+            myButton.setOnClickListener {
+                Toast.makeText(this,
+                    "Button clicked index = " + id_, Toast.LENGTH_SHORT)
+                    .show();
+            }
         }
     }
 }
